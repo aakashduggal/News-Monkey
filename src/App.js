886f92c4dfaef917feb3a42532fdead9b43c7fad
Aakash Duggal <aakashduggal2407@gmail.com>
2026-01-07 16:28:29 +0530

    navbar included

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-  <>
-  </>
-  );
+import React, {Component} from 'react'
+
+export default class App extends Component{
+  
+   render(){
+    return (
+      <>
+      <Navbar/>
+      </>
+    )
+   }
 }
-
-export default App;
