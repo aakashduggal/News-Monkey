f0c816d1c9ac55225e24a7c5c8d94ea327ce71ac
Aakash Duggal <aakashduggal2407@gmail.com>
2026-01-08 14:27:49 +0530

    added next and previous button so that user can fetch the data(news) of next page

--- src/components/News.js
+++ src/components/News.js
@@ -5,12 +5,13 @@ import NewsItem from './NewsItem'
 export class News extends Component{
     
     async componentDidMount(){
-        let url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=d44a33970c9149078263b4ed2e8be883'
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=d44a33970c9149078263b4ed2e8be883&page=${this.state.page}`
         let data = await fetch(url)
         let parsedData = await data.json()
         console.log(parsedData)
         this.setState({
-            articles:parsedData.articles
+            articles:parsedData.articles,
+            totalResults:parsedData.totalResults
         })
     }
 
@@ -19,9 +20,38 @@ export class News extends Component{
      console.log("Constructor is called in News")
       this.state={
          articles: [],
-         loading: false
+         loading: false,
+         page:1
      }
     } 
+
+    handlePrevClick = async ()=>{
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=d44a33970c9149078263b4ed2e8be883&page=${this.state.page - 1}`
+        let data = await fetch(url)
+        let parsedData = await data.json()
+        console.log(parsedData)
+        this.setState({
+            articles:parsedData.articles,
+            page: this.state.page-1
+        })
+    }  
+    
+    handleNextClick = async ()=>{
+       if(this.state.page >= Math.ceil(this.state.totalResults/20)){
+
+       }
+       else{
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=d44a33970c9149078263b4ed2e8be883&page=${this.state.page+1}`
+        let data = await fetch(url)
+        let parsedData = await data.json()
+        console.log(parsedData)
+        this.setState({
+            articles:parsedData.articles,
+            page:this.state.page +1
+        })
+    }
+    } 
+
     render(){
       
         return(
@@ -35,8 +65,12 @@ export class News extends Component{
              <NewsItem title={element.title} dispcription={element.description} imageUrl={element.urlToImage} url={element.url}/>     
             </div>
             })}
-
             
+            <div className='container my-3 d-flex justify-content-between'>
+                <button type="button" disabled={this.state.page<=1} onClick={this.handlePrevClick} class="btn btn-dark">&larr; Previous</button>
+                <button type="button" onClick={this.handleNextClick} class="btn btn-dark">Next &rarr;</button>
+
+            </div>
             </div>
             </div>
             </>
